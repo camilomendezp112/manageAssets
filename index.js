50d968f8674514216920cb0d7f77424c837d7205
camilomendezp112 <camilomendezp112@gmail.com>
2026-05-03 21:31:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: "Hola desde Lambda 🚀"
-  };
+  try {
+    const body = JSON.parse(event.body);
+
+    const { email, password } = body;
+
+    // Validación básica
+    if (!email || !password) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Email y password son requeridos"
+        })
+      };
+    }
+
+    // Simulación de usuario (luego esto será DynamoDB)
+    if (email === "admin@test.com" && password === "123456") {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Login exitoso",
+          token: "fake-jwt-token"
+        })
+      };
+    }
+
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: "Credenciales inválidas"
+      })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Error interno",
+        error: error.message
+      })
+    };
+  }
 };
