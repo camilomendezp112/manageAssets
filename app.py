a2bd815e538cfe75efa14975775d32808ecc8f2f
camilomendezp112 <camilomendezp112@gmail.com>
2026-05-27 03:10:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def make_response(success, code, data):
         'headers': {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-            'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS'
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': '*'
         },
         'body': json.dumps({
             'success': success,
